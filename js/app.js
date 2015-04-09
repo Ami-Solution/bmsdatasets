29b5845480005a8866dd896a4352b53108b9f3e9
Bosco So <boscoso@pif-bosco-mac.local>
2015-04-09 01:53:17 -0700

    index.html display now driven completely by JSON

---
+++ js/app.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function() {
+    jQuery("#templates-container").load("templates/item.html");
+});
+
+jQuery(document).ready(function() {
+    var http = jQuery.ajax('data/datasets.json');
+    http.success(function(data) {
+        var datasets = data.datasets;
+        var categories = datasets.reduce(
+            function(hash, ds) {
+                var cat = ds.category;
+                if (undefined === hash[cat]) {
+                    hash[cat] = [];
+                }
+                hash[cat].push(ds);
+
+                return hash;
+            },
+            {}
+        );
+        var sorted_keys = (function(hash) {
+            var arry = [];
+            for (var key in hash) { arry.push(key) }
+            return arry.sort();
+        })(categories);
+
+        var source    = jQuery('#item-template').html();
+        var template  = Handlebars.compile(source);
+        var container = jQuery('.featured-datasets .wrapper');
+
+        sorted_keys.map(function(key) {
+            var subsets = categories[key];
+            var data = {category:key, datasets:subsets};
+            var rendered = template(data);
+
+            container.append(rendered);
+        });
+    });
+})
